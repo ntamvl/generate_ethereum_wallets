127f624d1413ce152bcc33d980a902f7dece49ff
Tam Nguyen <ntamvl@gmail.com>
2018-03-30 09:50:42 +0700

    update ...

--- gen.rb
+++ gen.rb
@@ -1,9 +1,9 @@
 require './lib.rb'
 
-# process_generating_wallets
+process_generating_wallets
 
 # for testing
-MAX_WALLET_COUNT.times.each do |index|
-  item = { address: SecureRandom.hex(16), password: WALLET_PASSWORD }
-  write_wallets_json_file(item)
-end
+# MAX_WALLET_COUNT.times.each do |index|
+#   item = { address: SecureRandom.hex(16), password: WALLET_PASSWORD }
+#   write_wallets_json_file(item)
+# end
